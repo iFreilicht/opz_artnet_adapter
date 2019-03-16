7b78fae421ce00dd56f02135dc98d87cbeffcb97
iFreilicht <felix.uhl@outlook.com>
2019-03-16 14:39:20 +0100

    Send Artnet data to other device

--- WiFiClient.ino
+++ WiFiClient.ino
@@ -7,81 +7,62 @@
  */
 
 #include <WiFi.h>
+#include <ArtnetWifi.h>
 
+// Wifi settings
 const char* ssid     = "my_wifi_ssid";
 const char* password = "my_wifi_password";
 
+// Artnet settings
+ArtnetWifi artnet;
+const int universe = 1;
 const char* host = "0.0.0.0";
 
 void setup()
 {
-    Serial.begin(115200);
-    delay(10);
+  Serial.begin(115200);
+  delay(10);
 
-    // We start by connecting to a WiFi network
+  // We start by connecting to a WiFi network
 
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-    WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  artnet.begin(host);
+  artnet.setLength(3);
+  artnet.setUniverse(universe);
 }
 
 int value = 0;
 
 void loop()
 {
-    delay(5000);
-    ++value;
+  uint8_t i;
+  uint8_t j;
 
-    Serial.print("connecting to ");
-    Serial.println(host);
-
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-    const int httpPort = 9090;
-    if (!client.connect(host, httpPort)) {
-        Serial.println("connection failed");
-        return;
+  // set the first 3 byte to all the same value. A RGB lamp will show a ramp-up white.
+  for (j = 0; j < 255; j++) {
+    for (i = 0; i < 3; i++) {
+      artnet.setByte(i, j);
     }
-
-    // We now create a URI for the request
-    String url = "/ola.html";
-
-    Serial.print("Requesting URL: ");
-    Serial.println(url);
-
-    // This will send the request to the server
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-        if (millis() - timeout > 5000) {
-            Serial.println(">>> Client Timeout !");
-            client.stop();
-            return;
-        }
-    }
-
-    // Read all the lines of the reply from server and print them to Serial
-    while(client.available()) {
-        String line = client.readStringUntil('\r');
-        Serial.print(line);
-    }
-
-    Serial.println();
-    Serial.println("closing connection");
+    // send out the Art-Net DMX data
+    artnet.write();
+    Serial.println(j);
+    delay(100);
+  }
 }
 
