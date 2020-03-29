834caa04d11138865a1a439fcba63dac8074d57b
Felix Uhl <felix.uhl@outlook.com>
2020-03-29 16:28:15 +0200

    Update default host and instructions

--- artnet_settings.h
+++ artnet_settings.h
@@ -9,4 +9,7 @@ const char* password = "my_wifi_password";
 // Artnet settings.
 // Do NOT use universe 0, that will fail silently in most cases
 const int universe = 1;
-const char* host = "0.0.0.0";
+// Set this to the IP or hostname you want to send to,
+// or the broadcast adress of your network.
+// See https://en.wikipedia.org/wiki/Broadcast_address#IP_networking
+const char* host = "192.168.1.255";
